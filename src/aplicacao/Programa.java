8bf8b235f1637dabd09c88063d015e82a071c3e4
Alexandre Losano <alexandrelosano@gmail.com>
2020-01-14 22:33:55 -0300

    Classe de entidade Vendedor

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,13 +1,18 @@
 package aplicacao;
 
+import java.util.Date;
+
 import modelo.entidade.Departamento;
+import modelo.entidade.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
 		Departamento obj = new Departamento(1, "Books");
+		Vendedor vend= new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(obj);
+		System.out.println(vend);
 		
 	}
 
